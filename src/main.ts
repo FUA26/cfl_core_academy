6a3b6eb1191bce0b3d1698dfcc5b8de58d089fb6
Fatih <fatihulil@gmail.com>
2024-05-04 23:33:13 +0700

    refactor: :memo: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllConfigType } from './configs/config.type';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,18 @@ async function bootstrap() {
       exclude: ['/'],
     },
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Naira API Explorer')
+    .setDescription(
+      'Explore the capabilities of our API with detailed endpoints information, request samples, and response schemas.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(configService.get('app.port', { infer: true }));
 }
 bootstrap();
