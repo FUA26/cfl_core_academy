d335cb89227ca722bc262a31b639bf69688cfc1f
Fatih <fatihulil@gmail.com>
2024-05-04 10:55:11 +0700

    refactor: :art: add nest config and chage folders stucture

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllConfigType } from './configs/config.type';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService) as ConfigService<AllConfigType>;
+  await app.listen(configService.get('app.port', { infer: true }));
 }
 bootstrap();
